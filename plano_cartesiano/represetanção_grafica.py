2b0e2102906c37ae4f483a8ccd49613cea40f237
Gabriel Fernandes <130801640+fernxndees@users.noreply.github.com>
2024-09-26 09:10:28 -0300

    Update represetanção_grafica.py

--- plano_cartesiano/represetanção_grafica.py
+++ plano_cartesiano/represetanção_grafica.py
@@ -23,7 +23,13 @@ pontos_x = [2]
 pontos_y = [3]
 
 
-plt.scatter(pontos_x, pontos_y, color='red', label='Pontos', s=100)  
+plt.scatter(pontos_x, pontos_y, color='red', label='Pontos', s=100)
+
+#dominio e imagem
+dominio = r"D = { x ∈ ℝ | -5 ≤ x ≤ 5 }"
+imagem = r"I = { f(x) ∈ ℝ | f(x) ≥ -4 }"  
+print(dominio)
+print(imagem)
 
 #Cria a imagem do plano cartesiano
 plt.savefig('plano_cartesiano.png', format='png')
